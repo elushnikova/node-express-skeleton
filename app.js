9fd1b9bdeb33109d792515502e54e61aacdde0e8
Yuriy Pliskovsky <zzaafamania@gmail.com>
2021-07-19 16:29:38 +0300

    Complete refactor

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import config from './middleware/config.js'
+import config from './config/config.js'
 import mainRouter from './routes/main.js'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
+// конфигурация приложения
 config(app)
 
+// роутинг
 app.use('/', mainRouter)
 
+// прослушивание порта приложения
 app.listen(port, () => console.log(`*Server started at ${port} port`))
---
+++ db/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const dbConnection = () => {
+  mongoose.connect(process.env.DB_URI, { keepAlive: true, useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+
+      // статус подключения БД
+      let dbStatus = ''
+
+      if (err) {
+        dbStatus = `*Error connecting to DB: ${err}\n****************************\n`
+      } else {
+        dbStatus = `*DB Connection: OK\n****************************\n`
+      }
+
+      console.log(dbStatus)
+    }
+  )
+}
+
+export default dbConnection
